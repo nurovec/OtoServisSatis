3c1e49a6daa6c229345f3377bbe7da97eeb4ecc9
Nur Yavuz <nuryavuzz@outlook.com>
2023-07-06 00:27:25 +0300

    Add project files.

---
+++ OtoServisSatis.Entities/Kullanici.cs
@@ -0,0 +1,17 @@
+﻿namespace OtoServisSatis.Entities
+{
+    public class Kullanici :IEntity
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Sifre { get; set; }
+        public bool AktifMi { get; set; }
+        public DateTime EklenmeTarihi { get; set; }
+        public int RolId { get; set; }
+        public virtual Rol Rol { get; set; }
+    }
+}
