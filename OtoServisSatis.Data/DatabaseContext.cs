c787fd818f2c660e06da0bd3bfbbb24a57e31e2e
Nur Yavuz <nuryavuzz@outlook.com>
2023-07-06 21:40:19 +0300

    OnConfiguring - OnModelCreating - Fluent API

--- OtoServisSatis.Data/DatabaseContext.cs
+++ OtoServisSatis.Data/DatabaseContext.cs
@@ -17,5 +17,36 @@ namespace OtoServisSatis.Data
         public DbSet<Rol> Roller { get; set; }
         public DbSet<Satis> Satislar { get; set; }
         public DbSet<Servis> Servisler { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;initial database=OtoServisSatisNetCore;integrated
+           security=True;MultipleActiveResultSets=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //fluent API
+            modelBuilder.Entity<Marka>().Property(m=>m.Adi).IsRequired().HasColumnType("nvarchar(50)");
+            modelBuilder.Entity<Rol>().HasData(new Rol
+            {
+                Id = 1,
+                Adi="Admin"
+            });
+            modelBuilder.Entity<Kullanici>().HasData(new Kullanici
+            {
+                Id = 1,
+                Adi = "Admin",
+                AktifMi = true,
+                EklenmeTarihi = DateTime.Now,
+                Email = "admin@otoservissatis.tc",
+                KullaniciAdi = "admin",
+                Sifre = "123456",
+                Rol = new Rol { Id = 1 },
+                RolId = 1,
+                Soyadi = "Admin",
+                Telefon = "0850",
+            }) ;
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
