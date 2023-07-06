f4d6022ff8762a9b4629d3efc7b4679759f9a9da
Nur Yavuz <nuryavuzz@outlook.com>
2023-07-06 20:39:43 +0300

    Data Annotation Kullanımı ve DbContext Oluşturma

--- OtoServisSatis.Entities/Kullanici.cs
+++ OtoServisSatis.Entities/Kullanici.cs
@@ -1,17 +1,25 @@
-﻿namespace OtoServisSatis.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OtoServisSatis.Entities
 {
     public class Kullanici :IEntity
     {
         public int Id { get; set; }
+        [StringLength(50)]
         public string Adi { get; set; }
+        [StringLength(50)]
         public string Soyadi { get; set; }
+        [StringLength(50)]
         public string Email { get; set; }
+        [StringLength(20)]
         public string Telefon { get; set; }
+        [StringLength(50)]
         public string KullaniciAdi { get; set; }
+        [StringLength(50)]
         public string Sifre { get; set; }
         public bool AktifMi { get; set; }
-        public DateTime EklenmeTarihi { get; set; }
+        public DateTime? EklenmeTarihi { get; set; }= DateTime.Now;
         public int RolId { get; set; }
-        public virtual Rol Rol { get; set; }
+        public virtual Rol? Rol { get; set; }
     }
 }
