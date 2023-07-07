940c67394f026190f98d9ec995ba6dd8b9fb45dc
Nur Yavuz <nuryavuzz@outlook.com>
2023-07-07 19:31:36 +0300

    Generic Repository - Interface Oluşturma

---
+++ OtoServisSatis.Data/Abstract/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace OtoServisSatis.Data.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T, bool>> expression);
+        T Get(Expression<Func<T, bool>> expression);
+        T Find(int id);
+        void add(T entity);
+        void update(T entity);
+        void delete(T entity);
+        int Save();
+        //Asenkron Metotlar 
+        Task<T> FindAsync(int id);
+        Task<T> GetAsync(Expression<Func<T, bool>> expression);
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task <int>SaveAsync();
+
+    }
+}
