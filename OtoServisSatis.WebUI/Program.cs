c95f4c1ce615a56269abe767afebdf3f066ec0da
Nur Yavuz <nuryavuzz@outlook.com>
2023-07-11 15:47:16 +0300

    Admin Panelini Oluşturma

--- OtoServisSatis.WebUI/Program.cs
+++ OtoServisSatis.WebUI/Program.cs
@@ -23,6 +23,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.MapControllerRoute(
+            name : "admin",
+            pattern : "{area:exists}/{controller=Main}/{action=Index}/{id?}"
+          );
 
 app.MapControllerRoute(
     name: "default",
