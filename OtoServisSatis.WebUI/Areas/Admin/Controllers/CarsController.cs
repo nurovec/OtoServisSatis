d06ceb2b268a2b47be8138ac03c8efecff866046
Nur Yavuz <nuryavuzz@outlook.com>
2023-08-09 13:40:28 +0300

    Admin Araç Listeleme

---
+++ OtoServisSatis.WebUI/Areas/Admin/Controllers/CarsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OtoServisSatis.Entities;
+using OtoServisSatis.Service.Abstract;
+
+namespace OtoServisSatis.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CarsController : Controller
+    {
+        private readonly IService<Arac> _service;
+
+        public CarsController(IService<Arac> service)
+        {
+            _service = service;
+        }
+
+        // GET: CarsController
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+        // GET: CarsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CarsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CarsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CarsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CarsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
