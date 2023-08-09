214b814c55d2b6b2cf03debfbffb9395dd304e59
Nur Yavuz <nuryavuzz@outlook.com>
2023-08-09 11:52:04 +0300

    Admin Marka Listeleme

---
+++ OtoServisSatis.WebUI/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OtoServisSatis.Entities;
+using OtoServisSatis.Service.Abstract;
+
+namespace OtoServisSatis.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+        private readonly IService<Marka> _service;
+
+        public BrandsController(IService<Marka> service)
+        {
+            _service = service;
+        }
+
+        // GET: BrandsController
+        public async Task<ActionResult> IndexAsync()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+        // GET: BrandsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BrandsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
