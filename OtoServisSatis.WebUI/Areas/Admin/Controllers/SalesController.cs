097588650671d69686d875e2d50b5e0bb8d137b8
Nur Yavuz <nuryavuzz@outlook.com>
2023-08-10 17:28:55 +0300

    Admin Satış Listeleme ve İkon Düzeltmeleri

---
+++ OtoServisSatis.WebUI/Areas/Admin/Controllers/SalesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OtoServisSatis.Entities;
+using OtoServisSatis.Service.Abstract;
+
+namespace OtoServisSatis.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SalesController : Controller
+    {
+        private readonly IService<Satis> _service;
+
+        public SalesController(IService<Satis> service)
+        {
+            _service = service;
+        }
+
+        // GET: SalesController
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = await _service.GetAllAsync();
+            return View(model);
+        }
+
+        // GET: SalesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SalesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SalesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SalesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SalesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SalesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SalesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
