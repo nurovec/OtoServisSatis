fe0fe1289d10873ff593740ba8329dc76097bc58
Nur Yavuz <nuryavuzz@outlook.com>
2023-08-17 18:16:21 +0300

    Araçlara Resim Özelliği Ekleme

---
+++ OtoServisSatis.Data/Migrations/20230817150714_AracResimEklendi.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtoServisSatis.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AracResimEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AracSorunu",
+                table: "Servisler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Musteriler",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resim1",
+                table: "Araclar",
+                type: "nvarchar(100)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resim2",
+                table: "Araclar",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resim3",
+                table: "Araclar",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 8, 17, 18, 7, 13, 954, DateTimeKind.Local).AddTicks(1465));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Resim1",
+                table: "Araclar");
+
+            migrationBuilder.DropColumn(
+                name: "Resim2",
+                table: "Araclar");
+
+            migrationBuilder.DropColumn(
+                name: "Resim3",
+                table: "Araclar");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AracSorunu",
+                table: "Servisler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Musteriler",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 8, 3, 13, 46, 20, 543, DateTimeKind.Local).AddTicks(4276));
+        }
+    }
+}
