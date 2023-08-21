e631b4bad1092f7ecd0304d7f11ea60c240f463e
Nur Yavuz <nuryavuzz@outlook.com>
2023-08-21 14:56:31 +0300

    Özel Araç Repository İle Araçları Markalarıyla Birlikte Çekme

---
+++ OtoServisSatis.Data/Abstract/ICarRepository.cs
@@ -0,0 +1,10 @@
+﻿using OtoServisSatis.Entities;
+
+namespace OtoServisSatis.Data.Abstract
+{
+    public interface ICarRepository : IRepository<Arac>
+    {
+        Task<IEnumerable<Arac>>GetCustomCarList();
+        Task<Arac> GetCustomCar(int id);
+    }
+}
---
+++ OtoServisSatis.Data/Concrete/CarRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OtoServisSatis.Data.Abstract;
+using OtoServisSatis.Entities;
+
+namespace OtoServisSatis.Data.Concrete
+{
+    public class CarRepository : Repository<Arac>, ICarRepository
+    {
+        public CarRepository(DatabaseContext context) : base(context)
+        {
+        }
+
+        public async Task<Arac> GetCustomCar(int id)
+        {
+            return await _dbSet.AsNoTracking().Include(x => x.Marka).FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<Arac>> GetCustomCarList()
+        {
+           return await _dbSet.AsNoTracking().Include(x=>x.Marka).ToListAsync();
+        }
+    }
+}
