f670f8333646ab0a9049b7b3d3c934d593d92c1a
Nur Yavuz <nuryavuzz@outlook.com>
2023-08-24 15:06:06 +0300

    Araç Detay Müşteri Bilgi Al Ekranı Backend

--- OtoServisSatis.WebUI/Controllers/AracController.cs
+++ OtoServisSatis.WebUI/Controllers/AracController.cs
@@ -1,33 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OtoServisSatis.Entities;
 using OtoServisSatis.Service.Abstract;
+using OtoServisSatis.Service.Concrete;
 
 namespace OtoServisSatis.WebUI.Controllers
 {
     public class AracController : Controller
     {
         private readonly ICarService _serviceArac;
+        private readonly IService<Musteri> _serviceMusteri;
 
-        public AracController(ICarService serviceArac)
+        public AracController(ICarService serviceArac, IService<Musteri> serviceMusteri)
         {
             _serviceArac = serviceArac;
+            _serviceMusteri = serviceMusteri;
         }
 
         public async Task<IActionResult> IndexAsync(int id)
         {
-           var model= await _serviceArac.GetCustomCar(id);
+            var model = await _serviceArac.GetCustomCar(id);
             return View(model);
         }
         [Route("tum-araclarimiz")]
         public async Task<IActionResult> List()
         {
-            var model = await _serviceArac.GetCustomCarList(c=>c.SatistaMi);
+            var model = await _serviceArac.GetCustomCarList(c => c.SatistaMi);
             return View(model);
         }
         public async Task<IActionResult> Ara(string q)
         {
             var model = await _serviceArac.GetCustomCarList(c => c.SatistaMi && c.Marka.Adi.Contains(q) || c.KasaTipi.Contains(q) || c.Modeli.Contains(q));
             return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> MusteriKayit(Musteri musteri)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _serviceMusteri.AddAsync(musteri);
+                    await _serviceMusteri.SaveAsync();
+                    return Redirect("/Arac/Index/" + musteri.AracId);
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata OLuştu!");
+                }
+            }
+            return View();
         }
     }
 }
