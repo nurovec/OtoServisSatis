5462b0f53e16c07d448b10112921018c38c7b5e6
Nur Yavuz <nuryavuzz@outlook.com>
2023-09-04 14:03:59 +0300

    Kullanıcılara Guid-Benzersiz Kimlik Oluşturma

---
+++ OtoServisSatis.Data/Migrations/20230904105419_UserGuidEklendi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtoServisSatis.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserGuidEklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserGuid",
+                table: "Kullanicilar",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EklenmeTarihi", "UserGuid" },
+                values: new object[] { new DateTime(2023, 9, 4, 13, 54, 19, 294, DateTimeKind.Local).AddTicks(207), new Guid("4f9e3f2e-65ba-479f-87db-fb3255828efe") });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserGuid",
+                table: "Kullanicilar");
+
+            migrationBuilder.UpdateData(
+                table: "Kullanicilar",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "EklenmeTarihi",
+                value: new DateTime(2023, 8, 20, 16, 9, 6, 859, DateTimeKind.Local).AddTicks(2083));
+        }
+    }
+}
--- OtoServisSatis.Entities/Kullanici.cs
+++ OtoServisSatis.Entities/Kullanici.cs
@@ -27,5 +27,6 @@ namespace OtoServisSatis.Entities
         public int RolId { get; set; }
         [Display(Name = "Kullanıcı Rolü")]
         public virtual Rol? Rol { get; set; }
+        public Guid? UserGuid { get; set; } = Guid.NewGuid();
     }
 }
