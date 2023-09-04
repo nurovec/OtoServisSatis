bd56419c2e79c45cc576643726d13c71167d5974
Nur Yavuz <nuryavuzz@outlook.com>
2023-09-04 15:40:41 +0300

    Kullanıcı Repository ve Servisini Oluşturma

---
+++ OtoServisSatis.Data/Concrete/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtoServisSatis.Data.Concrete
+{
+    internal class UserRepository
+    {
+    }
+}
